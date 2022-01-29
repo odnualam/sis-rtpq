55ef81078e272bd26cdfb8aa222fc5f33d9cbfff
Fransiscus Rolanda Malau <fransiscusrmalau@gmail.com>
2022-01-30 00:25:37 +0700

    remove Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/city/province_code/{province_code}', 'References\CityController')->name('city.list.json');
 Route::get('/district/city_code/{city_code}', 'References\DistrictController')->name('districts.list.json');
